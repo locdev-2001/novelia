d05dd0e66526903a101a940185acfa365c7bec02
vanvuong1118 <73210556+vanvuong1118@users.noreply.github.com>
2022-09-09 17:17:10 +0700

    9/9/2022

--- mang_xa_hoi/views.py
+++ mang_xa_hoi/views.py
@@ -14,9 +14,25 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
-    
+
     posts = Post.objects.all()
-    return render(request, 'home.html', {'user_profile': user_profile, 'posts':posts})
+
+    return render(request, 'home.html', {'user_profile': user_profile, 'posts': posts})
+
+
+@login_required(login_url='signin')
+def profile(request, pk):
+    user_object = User.objects.get(username=pk)
+    user_profile = Profile.objects.get(user=user_object)
+    user_posts = Post.objects.filter(user=pk)
+    user_post_length = len(user_posts)
+    context = {
+        'user_object': user_object,
+        'user_profile': user_profile,
+        'user_posts': user_posts,
+        'user_post_length': user_post_length,
+    }
+    return render(request, 'profile.html', context)
 
 
 @login_required(login_url='signin')
@@ -25,26 +41,29 @@ def upload(request):
         user = request.user.username
         image = request.FILES.get('image_upload')
         caption = request.POST['caption']
-        
-        new_post = Post.objects.create(user = user, image = image, caption = caption)
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
         new_post.save()
         return redirect('/')
     else:
         return redirect('/')
 
+
 @login_required(login_url='signin')
 def like_post(request):
     username = request.user.username
     post_id = request.GET.get('post_id')
-    
-    post = Post.objects.get(id = post_id)
-    
-    like_filter = LikePost.objects.filter(post_id = post_id, username = username).first()
-    
+
+    post = Post.objects.get(id=post_id)
+
+    like_filter = LikePost.objects.filter(
+        post_id=post_id, username=username).first()
+
     if like_filter == None:
-        new_like = LikePost.objects.create(post_id_id = post_id, username = username)
+        new_like = LikePost.objects.create(
+            post_id_id=post_id, username=username)
         new_like.save()
-        
+
         post.liked = post.liked+1
         post.save()
         return redirect('/')
@@ -54,9 +73,11 @@ def like_post(request):
         post.save()
         return redirect('/')
 
+
 @login_required(login_url='signin')
 def settings(request):
     user_profile = Profile.objects.get(user=request.user)
+
     if request.method == 'POST':
 
         if request.FILES.get('image'):
@@ -70,6 +91,9 @@ def settings(request):
             user_profile.profileImg = image
             user_profile.firstName = firstName
             user_profile.lastName = lastName
+            # User.objects.update(first_name=firstName, last_name=lastName)
+            # user.first_name = firstName
+            # user.last_name = lastName
             user_profile.bio = bio
             user_profile.location = location
             user_profile.save()
@@ -84,9 +108,13 @@ def settings(request):
             user_profile.profileImg = image
             user_profile.firstName = firstName
             user_profile.lastName = lastName
+            # User.objects.update(first_name=firstName, last_name=lastName)
+            # user.first_name = firstName
+            # user.last_name = lastName
             user_profile.bio = bio
             user_profile.location = location
             user_profile.save()
+            # User.save()
             # User.objects.update(email = email)
         return redirect('settings')
     return render(request, 'setting.html', {'user_profile': user_profile})
